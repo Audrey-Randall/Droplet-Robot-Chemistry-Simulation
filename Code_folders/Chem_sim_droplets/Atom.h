2de42f4cb7cac9a115e783299d7fa0455da5a57a
Audrey Randall <audrey.randall@colorado.edu>
2015-06-02 11:11:07 -0600

    Added the bare bones of a system to write a unique file to upload to each individual Droplet. Currently, it doesn't write to anything. Also fixed minor bugs.

--- Code_folders/Chem_sim_droplets/Atom.h
+++ Code_folders/Chem_sim_droplets/Atom.h
@@ -10,6 +10,7 @@ typedef struct
 	int valence[8];
 	char name[2]; //someone said this should be 3 (because strings (char arrays) in C have a \0 at the end?) http://stackoverflow.com/questions/8484952/printf-prints-junk-characters
 	int atomicNum;
+	char chemID[13];
 }Atom;
 
 #endif
--- Code_folders/Chem_sim_droplets/main.c
+++ Code_folders/Chem_sim_droplets/main.c
@@ -1,12 +1,10 @@
-/*
- * Created: 5/28/2015 1:11:07 PM
- *  Author: Audrey
- */ 
 #include "droplet_programs/test_code.h"
 #include "Atom.h"
 #include "stdbool.h"
 #include <stdio.h>
 #include "vector.h"
+
+int MY_CHEM_ID = 1; //This will eventually be written to the file by writeFiles.c!
 
 #define FOSC 16000000
 #define BAUD 115200
@@ -27,56 +25,61 @@ static FILE mystdout = FDEV_SETUP_STREAM(usart_putchar_printf, NULL, _FDEV_SETUP
 //bond type, diatomic, valence shell, name, atomic number
 //Bond type 0 = ionic
 //Diatomic 0 = false
-Atom* H = {1, 1, {1, 0, -1, -1, -1, -1, -1, -1}, {'H',' '}, 1};
-Atom* He = {1, 0, {1, 1, -1, -1, -1, -1, -1, -1}, {'H', 'e'}, 2};
-Atom* Li = {0, 0, {1, 0, 0, 0, 0, 0, 0, 0}, {'L','i'}, 3};
-Atom* Be = {0, 0, {1, 1, 0, 0, 0, 0, 0, 0}, {'B','e'}, 4};
+Atom H = {1, 1, {1, 0, -1, -1, -1, -1, -1, -1}, "H", 1, {1, 1, 1, 0, -1,-1, -1, -1, -1, -1, 'H', ' ', 1}};
+Atom He = {1, 0, {1, 1, -1, -1, -1, -1, -1, -1}, "He", 2, {1, 0, 1, 1, -1, -1, -1, -1, -1, -1, 'H', 'e', 2}};
+Atom Li = {0, 0, {1, 0, 0, 0, 0, 0, 0, 0}, "Li", 3, {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 'L', 'i', 3,}};
+Atom Be = {0, 0, {1, 1, 0, 0, 0, 0, 0, 0}, "Be", 4, {0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 'B', 'e', 4,}};
 	
 
 
-//arSize is the size of both arrays (they're the same length). atomArray is to be returned. numArray is the array of atomic numbers that must be converted to an array of Atom structs.
-vector initAtoms(int vectorSize, vector atomicNums)
+Atom getAtomFromAtomicNum(int atomicNum)
 {
-	vector atomVector;
-	vector_init(&atomVector);
-	for(int i = 0; i < vectorSize; i++)
-	{
-		int atomicNum = vector_get(&atomicNums, i);
-		switch(atomicNum){
-			case 1:
-			{
-				//Atom* H = {1, 1, {1, 0, -1, -1, -1, -1, -1, -1}, {'H',' '}, 1};
-				vector_add(&atomVector, H);
-				break;
-			}
-			case 2:
-			{
-				//Atom* He = {1, 0, {1, 1, -1, -1, -1, -1, -1, -1}, {'H', 'e'}, 2};
-				vector_add(&atomVector, He);
-				break;
-			}
-			case 3:
-			{
-				//Atom* Li = {0, 0, {1, 0, 0, 0, 0, 0, 0, 0}, {'L','i'}, 3};
-				vector_add(&atomVector, Li);
-				break;
-			}
-			case 4:
-			{
-				//Atom* Be = {0, 0, {1, 1, 0, 0, 0, 0, 0, 0}, {'B','e'}, 4};
-				vector_add(&atomVector, Be);
-				break;
-			}
-			default:
-				printf("No such element");
+	switch(atomicNum){
+		case 1:
+		{
+			//printf("case 1");
+			return H;
 		}
-	
-
-		//printf("Not enough elements exist or I didn't initialize enough. Here's what we've got. \n");
-		return atomVector;
+		case 2:
+		{
+			//printf("case 2");
+			return He;
+		}
+		case 3:
+		{
+			//printf("case 3");
+			return Li;
+		}
+		case 4:
+		{
+			//printf("case 4");
+			return Be;
+		}
+		default:
+			printf("No such element");
 	}
 }
 
+void loadSingleChemID(Atom ID)
+{
+	//Somehow store the Atom struct on the Droplet
+}
+
+void sendChemID()
+{
+	//send the character array associated with this atom to all nearby droplets?
+}
+
+vector recieveAllChemIDs()
+{
+	//no idea how to set this up, but somehow I want to return a vector of character arrays.
+}
+
+void react(vector AllChemIDs)
+{
+	//recieve the character arrays. Then do stuff!
+	
+}
 
 /*
  * Any code in this function will be run once, when the robot starts.
@@ -85,28 +88,8 @@ void init()
 {
 
 	set_rgb(10, 10, 255); //this is a test line
-	//Eventually, this vector should be created by user input. It has the atomic numbers of all the atoms we want to make.	
-	vector atomicNums;
-	int one = 1;
-	int two = 2;
-	int three = 3;
-	int four = 4;
-	vector_init(&atomicNums);
-	vector_add(&atomicNums, &one);
-	vector_add(&atomicNums, &two);
-	vector_add(&atomicNums, &three);
-	vector_add(&atomicNums, &four);
-	int vectorSize = vector_count(&atomicNums);
-	
-	//Convert vector of atomic numbers to vector of Atom structs
-	vector atomVector;
-	atomVector = initAtoms(vectorSize, atomicNums);
-	for(int i = 0; i < vectorSize; i++)
-	{
-		//vector_get returns *void. To typecast to *Atom, use (Atom*)vector_get(stuff)
-		printf("atomArray[i].name = %d \n", ((Atom*)vector_get(&atomVector, i))->atomicNum);
-	}
-	
+	Atom myID = getAtomFromAtomicNum(MY_CHEM_ID);
+	printf("this.name = %s \n", myID.name);
 }
 
 /*
@@ -115,6 +98,7 @@ void init()
 void loop()
 {
 	//set_rgb(255, 255, 255);
+	//wait for half a second?
 }
 
 /*
@@ -123,6 +107,6 @@ void loop()
  */
 void handle_msg(ir_msg* msg_struct)
 {
-	
+	//react();
 }
 
